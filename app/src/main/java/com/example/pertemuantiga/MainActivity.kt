c4a9fe2107a0d932f0fc264c4e77be6082f21275
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-10-19 11:25:12 +0700

    memanggil gambar menggunakan fungsi image

--- app/src/main/java/com/example/pertemuantiga/MainActivity.kt
+++ app/src/main/java/com/example/pertemuantiga/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,6 +61,9 @@ fun BasicCompose(
             fontSize = 50.sp,
         )
 
+        Image(painter = painterResource(id =R.drawable.fifa),
+            contentDescription = null
+        )
     }
 }
 
