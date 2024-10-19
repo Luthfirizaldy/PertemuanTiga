2099f202247d48908d7af1c05a463d925d156927
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-10-19 11:20:39 +0700

    menambah text Indonesia dan memberi style text seperti size,weight, dan warna

--- app/src/main/java/com/example/pertemuantiga/MainActivity.kt
+++ app/src/main/java/com/example/pertemuantiga/MainActivity.kt
@@ -13,7 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.pertemuantiga.ui.theme.PertemuanTigaTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +44,16 @@ fun BasicCompose(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
-    ){}
+    ){
+        Text(
+            text = "INDONESIA",
+            style = TextStyle(
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red,
+            )
+        )
+    }
 }
 
 @Composable
