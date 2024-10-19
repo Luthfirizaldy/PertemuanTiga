418d3d30fa263fd7e5ebc72eefe60541145ae392
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-10-19 11:34:53 +0700

    ok

--- app/src/main/java/com/example/pertemuantiga/MainActivity.kt
+++ app/src/main/java/com/example/pertemuantiga/MainActivity.kt
@@ -86,19 +86,3 @@ fun BasicCompose(
         )
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PertemuanTigaTheme {
-        Greeting("Android")
-    }
-}
